f01fc0c246af32c6e5218e051b0d4b51519078cc
carloscielo <barcafan012@gmail.com>
2026-01-03 01:49:35 -0700

    finished basic math opps

--- CrashCourse/math.cpp
+++ CrashCourse/math.cpp
@@ -5,6 +5,34 @@ int main () {
     int x = 10;
     int y = 3;
     int z = 0;
-    z = x+y;
-    std::cout << z;
+// General Operations
+    //z = x+y;
+    //z = x-y;
+    //z = x*y;
+    //z = x/y;
+    //std::cout << z;
+
+// Division, with floating point calc:
+    ////below is demonstrated that to divide at least one number divided must be a double
+    ////to use the modulus for the remainder all numbers must be ints.
+    //double g = y;
+    //double p = x/g;   
+    //std::cout << "the quotient is p = " << p << std::endl;
+    //int i = x % y;
+    //std::cout << "the remainder is i = " << i << std::endl;
+
+// Variable Mod Operations
+    ////suppose you want to increment x
+
+    //// x = x + 1; = x++; THIS IS THE INCREMENT OPERATOR
+    //// WHEN INCREMENTER (++) is suffix:
+    //int b = x++;
+    //std::cout << "the original value (x) is incrimented:" << x << std::endl;
+    //std::cout << "the original value (x) is assigned to (b):" << b << std::endl;
+
+    //// WHEN INCREMENTER (++) is preffix:
+    int b = ++x;
+    std::cout << "the original value (x) is incrimented:" << x << std::endl;
+    std::cout << "the incremented value (x) is assigned to (b):" << b << std::endl;
+
 }
